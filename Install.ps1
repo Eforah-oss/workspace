fba053ee8374dc443b2a8f3807692f5ed5774a17
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2023-11-15 16:41:15 +0100

    Fix Windows installation

--- Install.ps1
+++ Install.ps1
@@ -1,13 +1,12 @@
 $ModuleRoot = "$(
-  if ($PSVersionTable.Platform -eq "Windows") {
-    "$([Environment]::GetFolderPath("MyDocuments"))/PowerShell"
+  if (([System.Environment]::OSVersion.Platform -eq "Win32NT") `
+        -or ($PSVersionTable.Platform -eq "Windows")) {
+    "$([Environment]::GetFolderPath("MyDocuments"))/WindowsPowerShell"
   }
   else {
     "$([Environment]::GetFolderPath('LocalApplicationData'))/powershell"
   }
 )/Modules"
 
-if (!(Test-Path "$ModuleRoot/Workspace")) {
-  New-Item -Type Directory "$ModuleRoot/Workspace" 1>$null
-}
+New-Item -Type Directory -Force "$ModuleRoot/Workspace" 1>$null
 Copy-Item Workspace.psm1 "$ModuleRoot/Workspace/Workspace.psm1"
